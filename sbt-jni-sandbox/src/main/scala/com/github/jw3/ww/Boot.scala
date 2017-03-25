4e5b360bbcf2b86d05052603337d35e80ec2d09d
John Wass <jwass3@gmail.com>
2017-03-25 08:30:17 -0400

    add a pthread to simulate the alerting

--- sbt-jni-sandbox/src/main/scala/com/github/jw3/ww/Boot.scala
+++ sbt-jni-sandbox/src/main/scala/com/github/jw3/ww/Boot.scala
@@ -7,9 +7,15 @@ object Boot extends App {
 
   // add a callback
   MockPigpio.addCallback(1, new Callback {
-    def call(gpio: Int, level: Int, tick: Long) = println(s"Called $gpio $level $tick")
+    var cnt = 0
+    def call(gpio: Int, level: Int, tick: Long) = {
+      if (cnt % 100000 == 0) printf("%d12\t%d %d %d\n", cnt, gpio, level, tick)
+      cnt += 1
+    }
   })
 
   // simulate a gpio event
   MockPigpio.inject(1, 1)
+
+  Thread.sleep(5000)
 }
--- sbt-jni-sandbox/src/native/pigpio.c
+++ sbt-jni-sandbox/src/native/pigpio.c
@@ -1,20 +1,32 @@
 #include <strings.h>
 #include <jni.h>
 #include <sys/time.h>
+#include <pthread.h>
+#include <stdio.h>
 
 #include "com_github_jw3_ww_MockPigpio__.h"
 
 #define PI_MAX_GPIO 10
 
+static pthread_t pthAlert;
+static JavaVM* pthread_jvm = NULL;
+
 jobject callbacks[PI_MAX_GPIO];
 
 long getMicrotime();
 void call(int, int, long, jobject, JNIEnv*);
+static void* pthAlertThread(void*);
 
 JNIEXPORT void JNICALL Java_com_github_jw3_ww_MockPigpio_00024_initialize
   (JNIEnv* env, jobject o)
 {
   bzero(callbacks, sizeof(callbacks));
+
+  int sz = 0;
+  JNI_GetCreatedJavaVMs(&pthread_jvm, 1, &sz);
+
+  int i=0;
+  pthread_create(&pthAlert, NULL, pthAlertThread, &i);
 }
 
 JNIEXPORT void JNICALL Java_com_github_jw3_ww_MockPigpio_00024_addCallback
@@ -41,4 +53,19 @@ long getMicrotime() {
 	struct timeval currentTime;
 	gettimeofday(&currentTime, NULL);
 	return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
+}
+
+static void* pthAlertThread(void* x)
+{
+  JNIEnv* env = 0;
+  (*pthread_jvm)->AttachCurrentThread(pthread_jvm, (void**) &env, NULL);
+
+  while(1) {
+    int i = 0;
+    int t = getMicrotime();
+    for(; i<PI_MAX_GPIO; ++i) {
+      if(0 != callbacks[i])
+        call(i, 0, t, callbacks[i], env);
+     }
+  }
 }
