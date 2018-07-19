8cc47e3e72b284eaa35f179c6935974c976678b6
John Wass <jwass3@gmail.com>
2018-07-19 03:04:30 -0400

    updates to javacpp test

--- javacpp-test-case/src/main/scala/com/github/jw3/pigpio/Boot.scala
+++ javacpp-test-case/src/main/scala/com/github/jw3/pigpio/Boot.scala
@@ -5,32 +5,63 @@ import org.bytedeco.javacpp.pigpio
 import org.bytedeco.javacpp.pigpio.gpioAlertFunc_t
 
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 object Boot extends App {
   implicit val system = ActorSystem("crash-test")
 
-  val pin = args.headOption.map(_.toInt).getOrElse(1)
+  val listenOn = 4
+  val publishFrom = 3
 
   pigpio.gpioInitialise()
 
-  val a = system.actorOf(Props[Receiver])
-  val l = new foo(a)
+  val s = system.actorOf(Sender.props(publishFrom))
+  val r = system.actorOf(Props[Receiver])
+  val l = F(r)
 
-  println(s"adding listener, pin $pin")
-  pigpio.gpioSetAlertFunc(pin, l)
-  println("added listener")
+  println(s"adding listener, pin $listenOn")
+  pigpio.gpioSetAlertFunc(listenOn, l)
+
+  println(s"adding publisher, pin $publishFrom")
+  system.scheduler.schedule(1.second, 1.milli, s, Sender.DoIt)
 
   Await.ready(system.whenTerminated, Duration.Inf)
 }
 
-class foo(ref: ActorRef) extends gpioAlertFunc_t {
-  override def call(gpio: Int, level: Int, tick: Int) = ref ! s"TICK $tick"
+object F {
+  def apply(ref: ActorRef) = new F(ref)
+}
+
+class F(ref: ActorRef) extends gpioAlertFunc_t {
+  override def call(gpio: Int, level: Int, tick: Int): Unit =
+    ref ! s"gpio $gpio received $level at $tick"
+}
+
+object Sender {
+  def props(pin: Int) = Props(new Sender(pin))
+
+  case object DoIt
+}
+
+class Sender(pin: Int) extends Actor {
+  def low: Receive = {
+    case Sender.DoIt ⇒
+      pigpio.gpioWrite(pin, 0)
+      context.become(low)
+  }
+
+  def high: Receive = {
+    case Sender.DoIt ⇒
+      pigpio.gpioWrite(pin, 1)
+      context.become(high)
+  }
+
+  def receive: Receive = low
 }
 
 class Receiver extends Actor {
-  def receive = {
+  def receive: Receive = {
     case s ⇒ println(s)
   }
 }
